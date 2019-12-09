c7cd9f1fbfe070d8445d40b93090af0e80a01f92
Antonio Correia <antonio.alves.correia@gmail.com>
2019-12-09 13:18:46 -0300

    adding datatable

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -45,6 +45,7 @@ namespace OdeToFood
 
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +72,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
